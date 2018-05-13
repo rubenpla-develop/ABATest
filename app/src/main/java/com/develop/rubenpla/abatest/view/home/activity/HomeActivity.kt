bf0a1c0eb5e5ce59e010ffcdba6d7d8e6e9323be
Ruben Pla <rubenpla.develop@gmail.com>
2018-05-13 14:56:01 +0200

    [Activity] - Created BaseActivity class as base for upcoming activities on project            - HomeActivity  nowextending from BaseActivity class [REFACTOR] Activities moved to its corresponding packages.

--- app/src/main/java/com/develop/rubenpla/abatest/view/home/activity/HomeActivity.kt
+++ app/src/main/java/com/develop/rubenpla/abatest/view/home/activity/HomeActivity.kt
@@ -1,9 +1,11 @@
-package com.develop.rubenpla.abatest
+package com.develop.rubenpla.abatest.view.home.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.develop.rubenpla.abatest.R
+import com.develop.rubenpla.abatest.view.base.activity.BaseActivity
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
