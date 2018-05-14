6f23abb665ef63aabd70df346e0ef6053809740b
Ruben Pla <rubenpla.develop@gmail.com>
2018-05-14 13:46:46 +0200

    [UI] - fix on some id names      - CsvAdapter created in order to fill list [UI/Model] CsvItemModel pojo class created

---
+++ app/src/main/java/com/develop/rubenpla/abatest/model/CsvItemModel.kt
@@ -0,0 +1,3 @@
+package com.develop.rubenpla.abatest.model
+
+class CsvItemModel(val title : String?, val description : String?, val imageUrl : String?)
---
+++ app/src/main/java/com/develop/rubenpla/abatest/view/home/adapter/CsvAdapter.kt
@@ -0,0 +1,69 @@
+package rubenpla.develop.privtmdbendlesslist.ui.adapter
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.develop.rubenpla.abatest.R
+import com.develop.rubenpla.abatest.model.CsvItemModel
+import kotlinx.android.synthetic.main.item_list.view.*
+
+class CsvAdapter (private val list : MutableList<CsvItemModel>,
+                        private val listener : CsvItemModel)
+    : RecyclerView.Adapter<CsvAdapter.ItemViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        return ItemViewHolder(LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_list, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = list.get(position)
+        holder.title.text = item.title
+        holder.description.text = item.description
+        //TODO picasso imageloading
+        // holder.image
+    }
+
+    /**
+     *    HELPERS
+     **/
+
+    fun getItem(position: Int): CsvItemModel? {
+        return list[position]
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    fun add(item : CsvItemModel) {
+        list.add(item)
+        notifyItemInserted(list.size -1)
+    }
+
+    fun addAll(movies: ArrayList<CsvItemModel>) {
+        list.addAll(movies)
+        notifyItemRangeChanged( movies.size,list.size -1)
+    }
+
+    fun remove(position: Int) {
+        list.removeAt(position)
+        notifyItemRemoved(list.size)
+    }
+
+    fun clear() {
+        list.clear()
+        notifyDataSetChanged()
+    }
+
+    /**
+     *    HOLDERS
+     **/
+    class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val title = itemView.item_list_title!!
+        val description = itemView.item_list_description!!
+        val image = itemView.item_list_card_view_image_view!!
+    }
+}
